727e13dfb8ac8487952d4106f2e8eb3021f3eac4
shariard58 <shariard58@gmail.com>
2023-08-24 03:37:47 +0600

    the final delete operation

--- app.js
+++ app.js
@@ -52,6 +52,63 @@ app.get("/getStudent/:id",(req,res)=>
         })
 })
 
+// Put Request
+
+app.put("/update",(req, res) => 
+{
+    const info = req.body;
+    console.log(info.id)
+    db.readData().then(data=>
+        {
+            console.log(data)
+            const i = data.findIndex(s=>s.id == info.id)
+            if(i<=-1)
+            {
+                res.status(404).send("No student found with this id")
+                res.end()
+            }
+           else 
+           {
+             data[i]= info;
+             db.writeData(data).then(feedback => 
+                {
+                    res.send(info)
+                })
+           }
+
+        })
+})
+
+// delete Request
+
+app.delete("/delete/:id",(req, res)=>
+{
+    const id = parseInt(req.params.id)
+    db.readData().then(data => 
+        {
+            const index = data.findIndex(s => s.id==id)
+            if(index<=-1)
+            {
+                res.status(404).send("No id mathched with this id")
+                res.end()
+            }
+
+            else 
+            {
+                const newData = data.filter(s=> s.id != id)
+                console.log(newData)
+                db.writeData(newData).then(x => 
+                    {
+                        res.send("Delted")
+                        res.end()
+                    })
+                res.end()
+            }
+        })
+    // console.log(id)
+    // res.end()
+})
+
 app.get("/another",(req, res)=>
 {
     res.send("This is antoher request")

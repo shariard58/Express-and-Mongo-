eae2657b32b7851ff8f651e853c9b5f8291385cf
shariard58 <shariard58@gmail.com>
2023-08-24 22:57:06 +0600

    middleware : using middleware

--- app.js
+++ app.js
@@ -5,8 +5,19 @@ const PORT = 3000;
 
  const studentRouter = require("./routers/personRouter")
 app.use(express.json())
+app.use((req, res, next)=>
+{
+console.log("I am middleware 1")
+next()
+})
 
- app.use('/api/person', studentRouter)
+app.use((req, res, next)=>
+{
+    console.log("I am middleware two")
+    next()
+})
+
+app.use('/api/person', studentRouter)
 app.listen(PORT, ()=>
 {
     console.log("Server is running successfully")

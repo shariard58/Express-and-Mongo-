ded360171972ce07a315369f8ca3e8532b2d5001
shariard58 <shariard58@gmail.com>
2023-08-25 16:26:01 +0600

    third party middleware morgan

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const app = express()
 const db =require("./db")
+
+
+
 const PORT = 3000;
 
- const studentRouter = require("./routers/personRouter")
+ const studentRouter = require("./routers/personRouter");
+const morgan = require("morgan");
 app.use(express.json())
+
+app.use(morgan("dev"))
 app.use((req, res, next)=>
 {
 console.log("I am middleware 1")
